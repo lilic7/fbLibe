c15e0aee7c202c77e12eff14669ccb0816494a24
lilic7 <zubcolilian@mail.ru>
2020-05-04 18:06:05 +0300

    work on tkinter list of programs

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from time import strftime
 
 from obs import Obs
 from fbLive.live import FbLive
+from scheduleLive.scheduleLive import ScheduleLive
 
 root = tk.Tk()
 
-canvas1 = tk.Canvas(root, width=300, height=300)
+canvas1 = tk.Canvas(root, width=300, height=500)
 canvas1.pack()
 fbLive = FbLive("DevTest")
 
@@ -33,22 +34,37 @@ def time():
     time_label.after(1000, time)
 
 
+
+
 # Styling the label widget so that clock
 # will look more attractive
-time_label = tk.Label(root, font=('calibri', 40, 'bold'),
-               background='purple',
-               foreground='white')
+time_label = tk.Label(root, font=('calibri', 40, 'bold'), foreground='black')
 canvas1.create_window(150, 50, window=time_label)
 time()
 
 label = tk.Label(root, fg='green', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 100, window=label)
 
-start_btn = tk.Button(text='Start', command=start_live, bg='brown', fg='white')
+start_btn = tk.Button(text='Start', command=start_live, bg='brown', fg='white', font="16")
 canvas1.create_window(100, 150, window=start_btn)
 
-stop_btn = tk.Button(text='Stop', command=end_live, bg='brown', fg='white')
+stop_btn = tk.Button(text='Stop', command=end_live, bg='brown', fg='white', font="16")
 canvas1.create_window(200, 150, window=stop_btn)
+
+program_label = tk.Label(root, fg='green', font=('helvetica', 12, 'bold'), text="Program pentru azi")
+canvas1.create_window(150, 200, window=program_label)
+
+sc = ScheduleLive()
+program = sc.get_today_program()
+start_pos = 220
+current_date = strftime('%d.%m.%Y')
+i=0
+for item in program:
+    title = item["title"].replace("::date::", current_date)
+    label = tk.Label(root, fg='black', font=('helvetica', 10, 'normal'), text=f'{title}').grid(row=i, column=0)
+    canvas1.create_window(100, start_pos, window=label)
+    start_pos += 30
+    i += 1
 
 if __name__ == "__main__":
     root.mainloop()
---
+++ scheduleLive/scheduleLive.py
@@ -0,0 +1,34 @@
+import json
+import time
+
+
+class ScheduleLive:
+    def __init__(self):
+        self.settings = self.read_json()["data"]
+
+    @staticmethod
+    def read_json():
+        filename = './scheduleLive/settings.json'
+        try:
+            with open(filename) as settings:
+                return json.load(settings)
+        except FileNotFoundError:
+            print(f"File '{filename}' does not exists")
+
+    @staticmethod
+    def get_current_day():
+        return time.strftime("%a", time.gmtime())
+
+    def check_default_program(self, day):
+        default_days = self.settings[0]["default"]["days"]
+        return default_days.count(day)
+
+    def get_today_program(self):
+        day = self.get_current_day()
+        position = "default" if self.check_default_program(day) else day
+        return self.settings[0][position]["program"]
+
+
+if __name__ == "__main__":
+    sc = ScheduleLive()
+    print(sc.get_today_program())
